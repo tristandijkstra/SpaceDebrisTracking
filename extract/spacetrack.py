2eb206628b31052be385871ce075d77a0c3968b8
Tristan Dijkstra <tristan.dijkstra@gmail.com>
2022-12-19 14:50:47 +0100

    WIP query prototype

--- extract/spacetrack.py
+++ extract/spacetrack.py
@@ -4,6 +4,7 @@ import requests
 from datetime import datetime
 import os
 import io
+
 # import datetime
 
 
@@ -28,17 +29,19 @@ def getSpactrackClient(folder="keys"):
     else:
         raise ValueError("No key files given: add a file:'keys/spacetrack.txt'")
 
+
 def formatDates():
     pass
 
 
 def query(
-    username:str,
-    password:str,
+    username: str,
+    password: str,
     NORADid: int,
-    start:datetime,
-    end:datetime,
-    saveFolder = "data"
+    start: datetime,
+    end: datetime,
+    saveFolder="data",
+    forceRegen: bool = False
 ):
     if not os.path.exists(saveFolder):
         os.makedirs(saveFolder)
@@ -46,17 +49,16 @@ def query(
     startS = str(start)
     endS = str(end)
 
-    print(startS)
-    print(endS)
-
-    logonURL = 'https://www.space-track.org/ajaxauth/login'
+    logonURL = "https://www.space-track.org/ajaxauth/login"
     gpURL = "https://www.space-track.org/basicspacedata/query/class/gp_history/"
-    template = gpURL + f"NORAD_CAT_ID/{NORADid}/EPOCH/>2020-1-1,<2022-1-1/orderby/EPOCH asc/format/csv/"
-
+    template = (
+        gpURL
+        + f"NORAD_CAT_ID/{NORADid}/EPOCH/>2020-1-1,<2022-1-1/orderby/EPOCH asc/format/csv/"
+    )
 
     saveFilePath = f"{saveFolder}/{NORADid}.csv"
-    if not os.path.exists(saveFilePath):
-        creds = {"identity":username, "password":password}
+    if not os.path.exists(saveFilePath) or forceRegen:
+        creds = {"identity": username, "password": password}
         with requests.Session() as sesh:
             cookie = sesh.post(logonURL, data=creds)
 
@@ -66,18 +68,50 @@ def query(
                 raise ValueError(res, "GET fail on request")
             else:
                 pass
-            
-            if len(res.text) > 2:                
+
+            if len(res.text) > 2:
                 data = io.StringIO(res.text)
                 P = pd.read_csv(data)
-                P.to_csv(saveFilePath)
+                # P.to_csv(saveFilePath)
             else:
                 print(res.status_code)
                 raise RuntimeError("File is empty, may be API overload")
 
+            droplist = [
+                "CCSDS_OMM_VERS",
+                "COMMENT",
+                "CREATION_DATE",
+                "ORIGINATOR",
+                "REF_FRAME",
+                "TIME_SYSTEM",
+                "MEAN_ELEMENT_THEORY",
+                "EPHEMERIS_TYPE",
+                "CLASSIFICATION_TYPE",
+                "SITE",
+                "FILE",
+                "GP_ID",
+            ]
+            # print([u for u in P.columns if u not in droplist])
+
+            # dform = "%d-%m-%Y %H:%M:%S"
+            dform = "%Y-%m-%dT%H:%M:%S.%f"
+            P = (
+                P.drop(columns=droplist)
+                .assign(EPOCH=lambda x: pd.to_datetime(x.EPOCH, format=dform))
+                .assign(LAUNCH_DATE=lambda x: pd.to_datetime(x.LAUNCH_DATE, format=dform))
+                .assign(TLE_LINE1min1=lambda x: x.shift(1).TLE_LINE1)
+                .assign(TLE_LINE2min1=lambda x: x.shift(1).TLE_LINE2)
+                .assign(deltat=lambda x: (x.EPOCH - x.shift(1).EPOCH).dt.total_seconds())
+                # .assign(deltat=lambda x: x.deltat)
+                .drop(index=0)
+
+            )
+            P.to_csv(saveFilePath)
             return P
     else:
-        P = pd.read_csv(saveFilePath)
+        print(f"Data for {NORADid} already generated")
+        P = pd.read_csv(saveFilePath, index_col=0)
+
         # csv processing.
 
         return P
@@ -85,7 +119,10 @@ def query(
 
 if __name__ == "__main__":
     norads = [51092, 51062, 51081, 50987, 51032]
-    start = datetime(2016,1,1)
-    end = datetime(2023,1,1)
+    start = datetime(2016, 1, 1)
+    end = datetime(2023, 1, 1)
     username, password = getSpactrackClient()
-    query(username, password, 27386, start, end)
+    q = query(username, password, 27386, start, end, forceRegen=False)
+
+    print(q)
+    # print(q.dtypes)
